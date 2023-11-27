18ed5e520438d8c5663b46ead66ff4eab1346d1a
Jo�o Pedro Ribeiro Da Costa <52211545874>
2023-11-27 07:54:52 -0300

    updt

--- bank_web/src/components/Logo.tsx
+++ bank_web/src/components/Logo.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 interface Props{
     colorText: string
 }
@@ -5,7 +7,7 @@ interface Props{
 export default function Logo({colorText}:Props){
     return(
         <div className={`${colorText} w-1/6 text-center my-auto text-4xl font-bold cursor-pointer`}>
-            BANK<span className="text-[#FF1577]">!</span>
+            <Link href='/home'>BANK<span className="text-[#FF1577]">!</span></Link>
         </div>
     )
 }
--- bank_web/src/components/NavBar.tsx
+++ bank_web/src/components/NavBar.tsx
@@ -1,25 +1,15 @@
 "use client"
-import { useState } from 'react';
 import LinkText from "./LinkText";
 import Logo from "./Logo";
 
 export default function NavBar(){
-    const [menu, setMenu] = useState(false)
+
 
     return(
         <div className="w-full bg-[#232323] h-[calc(5rem)] flex flex-row">
             <Logo colorText="text-[#F4F4F4]"/>
             <div className="w-3/6"></div>
-            <div className="flex w-2/6 items-center sm:hidden">
-                <button 
-                    className="text-[#F4F4F4]"
-                    onClick={() => {setMenu(!menu)}}
-                >
-                    Menu
-                </button>
-            </div>
-            <div className=" hidden sm:flex w-2/6 flex-row items-center justify-center gap-20">
-                <LinkText link="/home" text="Home" colorText="text-[#F4F4F4]"/>
+            <div className="flex w-2/6 flex-row items-center justify-center gap-20">
                 <LinkText link="/log" text="Login" colorText="text-[#F4F4F4]"/>
             </div>
         </div>
